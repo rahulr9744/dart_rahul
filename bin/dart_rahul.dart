7b9e48f570acc269e9dd6a0b34caf5f1b0834038
rahulr9744 <rahulramesh0070@gmail.com>
2023-09-20 13:08:10 +0530

    commit

---
+++ bin/dart_rahul.dart
@@ -0,0 +1,15 @@
+
+
+void main(){
+  String name = 'rahul';
+  int age = 22;
+  int phone = 9744549976;
+  double cgp = 99.9;
+  String email = 'rahul@gmail.com';
+
+print('my name is $name');
+print('i am $age yrs old');
+print('my phone number is $phone');
+print('$cgp is my cgp rating in graduation');
+print('my emailid is $email');
+}
---
+++ test/dart_rahul_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_rahul/dart_rahul.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
