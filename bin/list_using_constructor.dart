e0c1f4d9626be1414f65c1fb5135ff687e56a987
rahulr9744 <rahulramesh0070@gmail.com>
2023-10-05 13:23:58 +0530

    commit

---
+++ bin/list_using_constructor.dart
@@ -0,0 +1,28 @@
+// void main(){
+//   var list1 = List.empty(growable: true);
+//   var list2 = [];
+//   list1.add(10);
+//   list1.addAll([100,76,768]);
+  
+//   var list3 = List.from(list1); //list.form() we are creating a list from another list or selected list
+
+//   list3.addAll([1,2,3,4,5]);
+  
+
+// var list4 = List.filled(10, 1,growable: true);
+// list4[3] = 6;
+// list4[4] = 2;
+// list4[6] = 7;
+
+// var list5 = List.of(list1);
+// var list6 = List.unmodifiable(list4);
+// var list7 = List.generate(10, (index) => 2*index);
+
+// print('list1 = $list1');
+// print('list2 = $list2');
+// print('list3 = $list3');
+// print('list4 = $list4');
+// print('list5 = $list5');
+// print('list6 = $list6');
+// print('list7 = $list7');
+// }
---
+++ bin/map_using_constructor.dart
@@ -0,0 +1,24 @@
+// void main(){
+//   var m1 = {};
+//   var m2 = Map();
+//   var l1 = [1,2,3,4,5];
+//   var s1 = {10,20,30,40,50};
+
+//   m2['key 1'] = 'value 1';
+//   m2['key 2'] = 'value 2';
+
+//   Map m3 = Map.fromEntries(m2.entries);
+//   m3['keys 3'] = 'value 3';
+
+//   Map m4 = Map.fromIterable(l1);
+//   Map m5 = Map.fromIterables(l1, s1);
+
+//   print(m1);
+//   print(m2);
+//   print(m3);
+//   print(m4);
+//   print(m5);
+
+
+
+// }
